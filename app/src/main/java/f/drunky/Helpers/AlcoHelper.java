471f35756db86b8e9cf99f9f2771c808ad6e465a
AZhloba <AZhloba@tut.by>
2018-10-30 21:30:30 +0300

    CanDriveIn calculation.

--- app/src/main/java/f/drunky/Helpers/AlcoHelper.java
+++ app/src/main/java/f/drunky/Helpers/AlcoHelper.java
@@ -12,52 +12,6 @@ public class AlcoHelper {
     static double K = 0.0056;
     static double Betta60 = 0.11;
 
-    public static double calcMille(List<DrunkItem> log, UserProfile userProfile, Date curTime) {
-        double r = 0.65;
-        double m = 65;
-
-        if (log.size() == 0)
-        {
-            return 0;
-        }
-        else
-        {
-            if (userProfile.isFilled())
-            {
-                r = userProfile.Gender == GenderHelper.Male.Code ? 0.7 : 0.6;
-                m = userProfile.Weight;
-            }
-
-            double c = 0;
-            Date time = log.get(0).getUseTime();
-            for (DrunkItem logItem: log)
-            {
-                if (logItem.getUseTime().getTime() > curTime.getTime()) {
-                    break;
-                }
-
-                long ds = TimeUnit.MILLISECONDS.convert(logItem.getUseTime().getTime() - time.getTime(), TimeUnit.MILLISECONDS);
-                double c_cur = c - Betta60 * ds / (60d * 60d * 1000d);
-                if (c_cur < 0) {
-                    c_cur = 0;
-                }
-
-                double A = logItem.getAlcohol() * logItem.getVolume() * K;
-                double C = A / (m * r);
-
-                c = c_cur + C;
-                time = logItem.getUseTime();
-            }
-
-            long ds = TimeUnit.MILLISECONDS.convert(curTime.getTime() - time.getTime(), TimeUnit.MILLISECONDS);
-            double c_cur = c - Betta60 * ds / (60d * 60d * 1000d);
-            if (c_cur < 0) {
-                c_cur = 0;
-            }
-
-            return c_cur;
-        }
-    }
 
     public static State calcState(List<DrunkItem> log, UserProfile userProfile, Date curTime) {
         double r = 0.65;
@@ -104,6 +58,7 @@ public class AlcoHelper {
 
             State state = new State();
             state.Mille = c_cur;
+            state.CanDriveInHours = c_cur / Betta60;
             return state;
         }
     }
--- app/src/test/java/f/drunky/AcloModelUnitTest.java
+++ app/src/test/java/f/drunky/AcloModelUnitTest.java
@@ -2,10 +2,12 @@ package f.drunky;
 
 import org.junit.Test;
 
+import java.sql.Time;
 import java.util.Date;
 import java.util.ArrayList;
 
 import f.drunky.Entity.DrunkItem;
+import f.drunky.Entity.State;
 import f.drunky.Helpers.AlcoHelper;
 import f.drunky.Helpers.TimeHelper;
 import f.drunky.mvp.models.UserProfile;
@@ -21,7 +23,9 @@ public class AcloModelUnitTest {
         UserProfile userProfile = GetUnfilledUserProfile();
         ArrayList<DrunkItem> log = new ArrayList<>();
 
-        assertEquals(0, AlcoHelper.calcMille(log, userProfile, TimeHelper.now()), DIF_DELTA);
+        State state = AlcoHelper.calcState(log, userProfile, TimeHelper.now());
+        assertEquals(0, state.Mille, DIF_DELTA);
+        assertEquals(0, state.CanDriveInHours, DIF_DELTA);
     }
 
     @Test
@@ -32,9 +36,19 @@ public class AcloModelUnitTest {
         Date useTime = new Date(0);
         log.add(new DrunkItem(useTime, null, null, 40, 100));
 
-        assertEquals(0.530177515, AlcoHelper.calcMille(log, userProfile, useTime), DIF_DELTA);
-        assertEquals(0.511844181, AlcoHelper.calcMille(log, userProfile, new Date(10 * 60 * 1000)), DIF_DELTA);
-        assertEquals(0, AlcoHelper.calcMille(log, userProfile, new Date(290 * 60 * 1000)), DIF_DELTA);
+        State state;
+
+        state = AlcoHelper.calcState(log, userProfile, useTime);
+        assertEquals(0.530177515, state.Mille, DIF_DELTA);
+        assertEquals(4.819795589, state.CanDriveInHours, DIF_DELTA);
+
+        state = AlcoHelper.calcState(log, userProfile, new Date(10 * 60 * 1000));
+        assertEquals(0.511844181, state.Mille, DIF_DELTA);
+        assertEquals(4.653128922, state.CanDriveInHours, DIF_DELTA);
+
+        state = AlcoHelper.calcState(log, userProfile, new Date(290 * 60 * 1000));
+        assertEquals(0, state.Mille, DIF_DELTA);
+        assertEquals(0, state.CanDriveInHours, DIF_DELTA);
     }
 
     @Test
@@ -45,12 +59,12 @@ public class AcloModelUnitTest {
         log.add(new DrunkItem(new Date(0), null, null, 5, 250));
         log.add(new DrunkItem(new Date(30 * 60 * 1000), null, null, 5, 250));
 
-        assertEquals(0.165680473, AlcoHelper.calcMille(log, userProfile, new Date(0)), DIF_DELTA);
-        assertEquals(0.14734714, AlcoHelper.calcMille(log, userProfile, new Date(10 * 60 * 1000)), DIF_DELTA);
-        assertEquals(0.276360947, AlcoHelper.calcMille(log, userProfile, new Date(30 * 60 * 1000)), DIF_DELTA);
-        assertEquals(0.221360947, AlcoHelper.calcMille(log, userProfile, new Date(60 * 60 * 1000)), DIF_DELTA);
-        assertEquals(0.111360947, AlcoHelper.calcMille(log, userProfile, new Date(120 * 60 * 1000)), DIF_DELTA);
-        assertEquals(0, AlcoHelper.calcMille(log, userProfile, new Date(190 * 60 * 1000)), DIF_DELTA);
+        assertEquals(0.165680473, AlcoHelper.calcState(log, userProfile, new Date(0)).Mille, DIF_DELTA);
+        assertEquals(0.14734714, AlcoHelper.calcState(log, userProfile, new Date(10 * 60 * 1000)).Mille, DIF_DELTA);
+        assertEquals(0.276360947, AlcoHelper.calcState(log, userProfile, new Date(30 * 60 * 1000)).Mille, DIF_DELTA);
+        assertEquals(0.221360947, AlcoHelper.calcState(log, userProfile, new Date(60 * 60 * 1000)).Mille, DIF_DELTA);
+        assertEquals(0.111360947, AlcoHelper.calcState(log, userProfile, new Date(120 * 60 * 1000)).Mille, DIF_DELTA);
+        assertEquals(0, AlcoHelper.calcState(log, userProfile, new Date(190 * 60 * 1000)).Mille, DIF_DELTA);
     }
 
 
