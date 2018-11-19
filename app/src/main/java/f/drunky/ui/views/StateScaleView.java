a8b2cc12d2fe6630614d6e46013d5f00d91424d7
AZhloba <AZhloba@tut.by>
2018-11-19 23:02:26 +0300

    Improving state scale.

--- app/src/main/java/f/drunky/ui/views/StateScaleView.java
+++ app/src/main/java/f/drunky/ui/views/StateScaleView.java
@@ -15,6 +15,9 @@ import android.view.View;
 public class StateScaleView extends View {
     private static final int LINE_WIDTH = 6;
     private static final int LINE_WIDTH_DIV2 = 6;
+    private static final double HILL_PART = 2.0/5.0;
+
+    private static final int STATES_LINE_WIDTH = 4;
 
     private Path _backPath;
     private int _width;
@@ -48,9 +51,9 @@ public class StateScaleView extends View {
 
         _backPath = new Path();
         _backPath.moveTo(LINE_WIDTH_DIV2, _height - LINE_WIDTH_DIV2);
-        _backPath.lineTo(LINE_WIDTH_DIV2, _height * 1 / 3 + LINE_WIDTH_DIV2);
+        _backPath.lineTo(LINE_WIDTH_DIV2, (int)(_height * HILL_PART + LINE_WIDTH_DIV2));
         _backPath.lineTo(_width / 2, LINE_WIDTH_DIV2);
-        _backPath.lineTo(_width - LINE_WIDTH_DIV2, _height * 1 / 3 + LINE_WIDTH_DIV2);
+        _backPath.lineTo(_width - LINE_WIDTH_DIV2, (int)(_height * HILL_PART + LINE_WIDTH_DIV2));
         _backPath.lineTo(_width - LINE_WIDTH_DIV2, _height - LINE_WIDTH_DIV2);
         _backPath.close();
 
@@ -68,34 +71,45 @@ public class StateScaleView extends View {
 
         canvas.drawPath(_backPath, _backPaint);
 
+        Paint statePaint = new Paint();
+        statePaint.setShader(new LinearGradient(0, 0, _width, getHeight(), Color.parseColor("#FF80AB"), Color.parseColor("#C51162"), Shader.TileMode.MIRROR));
+
         int x = _state * _width / 100;
         Path statePath;
         if (_state > 0) {
             if (_state <= 50) {
-                int y = (int) (_height / 3d * (50 - _state) / 50);
+                int y = (int) (_height * HILL_PART * (50 - _state) / 50);
 
                 statePath = new Path();
                 statePath.moveTo(LINE_WIDTH_DIV2, _height - LINE_WIDTH_DIV2);
-                statePath.lineTo(LINE_WIDTH_DIV2, _height * 1 / 3 + LINE_WIDTH_DIV2);
+                statePath.lineTo(LINE_WIDTH_DIV2, (int)(_height * HILL_PART + LINE_WIDTH_DIV2));
                 statePath.lineTo(x, y + LINE_WIDTH_DIV2);
                 statePath.lineTo(x, _height - LINE_WIDTH_DIV2);
                 statePath.close();
-            }
-            else {
-                int y = (int) (_height / 3d * (_state - 50) / 50);
+            } else {
+                int y = (int) (_height * HILL_PART * (_state - 50) / 50);
 
                 statePath = new Path();
                 statePath.moveTo(LINE_WIDTH_DIV2, _height - LINE_WIDTH_DIV2);
-                statePath.lineTo(LINE_WIDTH_DIV2, _height * 1 / 3 + LINE_WIDTH_DIV2);
+                statePath.lineTo(LINE_WIDTH_DIV2, (int)(_height * HILL_PART + LINE_WIDTH_DIV2));
                 statePath.lineTo(_width / 2, LINE_WIDTH_DIV2);
                 statePath.lineTo(x, y + LINE_WIDTH_DIV2);
                 statePath.lineTo(x, _height - LINE_WIDTH_DIV2);
                 statePath.close();
             }
 
-            Paint statePaint = new Paint();
-            statePaint.setShader(new LinearGradient(0, 0, _width, getHeight(), Color.parseColor("#FF80AB"), Color.parseColor("#C51162"), Shader.TileMode.MIRROR));
             canvas.drawPath(statePath, statePaint);
         }
+
+
+        Paint relaxPaint = _state < 25 ? _backPaint : statePaint;
+        relaxPaint.setStrokeWidth(STATES_LINE_WIDTH);
+        canvas.drawLine(_width / 4, _height - STATES_LINE_WIDTH, _width / 4, STATES_LINE_WIDTH, relaxPaint);
+        canvas.drawLine(_width / 4, STATES_LINE_WIDTH, STATES_LINE_WIDTH, STATES_LINE_WIDTH, relaxPaint);
+
+        Paint leglessPaint = _state < 75 ? _backPaint : statePaint;
+        relaxPaint.setStrokeWidth(STATES_LINE_WIDTH);
+        canvas.drawLine(_width * 3/4, _height - STATES_LINE_WIDTH, _width * 3/4, STATES_LINE_WIDTH, leglessPaint);
+        canvas.drawLine(_width * 3/4, STATES_LINE_WIDTH, _width - STATES_LINE_WIDTH, STATES_LINE_WIDTH, leglessPaint);
     }
 }
